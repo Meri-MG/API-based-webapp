dc53028c200ecd7809ebfd52c84ace6e8035c900
Gints Misins <88575586+GintsM@users.noreply.github.com>
2021-11-23 17:12:57 +0200

    Play popup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 
-const getLink = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2020-07-09&end_date=2020-07-14';
-const getImage = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2017-07-08';
+const getLink = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2020-07-15&end_date=2020-07-20';
+const getImage = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-07-21';
 const main = document.getElementById('addToScreen');
 
 const getScores = async (url) => {
@@ -9,7 +9,7 @@ const getScores = async (url) => {
   return response.json();
 };
 
-function addToScoreBord(img, title) {
+function addToScoreBord(img, title, index) {
   const div = document.createElement('div');
   div.classList.add('cardContainer');
   div.innerHTML = `
@@ -22,16 +22,17 @@ function addToScoreBord(img, title) {
           </span>
           <small>0 likes</small> 
         </div>
-        <input type="button" value="Comments">
+        <input type="button" value="Comments" id="${index}">
         `;
   main.appendChild(div);
 }
 
 function displayScores() {
   getScores(getLink)
-    .then((data) => {
-      main.innerHTML = '';
-      data.forEach((elem) => addToScoreBord(elem.hdurl, elem.title));
+    .then((data) => data.forEach((elem, index) => addToScoreBord(elem.hdurl, elem.title, index)))
+    .then(() => {
+      const lastComment = document.getElementById('5');
+      lastComment.addEventListener('click', displayImage);
     });
 }
 
@@ -40,34 +41,29 @@ displayScores();
 function displayPopup(img, title, description) {
   const popupDiv = document.createElement('div');
   popupDiv.classList.add('popupContainer');
-  popupDiv.innerHTML = `<div class="header-popup">
-    <img src="${img}" class="whatever" alt="close-icon">
-  </div>
-  <h2>${title}</h2>
-  <p class="description">${description}</p>
-  <div>
-      <h2 class="comments">Comments</h2>
-      <ul class="commentsContainer">
+  popupDiv.innerHTML = `
+   <div class="header-popup">
+     <img src="${img}" class="whatever" alt="close-icon">
+   </div>
+   <h2>${title}</h2>
+   <p class="description">${description}</p>
+   <div>
+       <h2 class="comments">Comments</h2>
+       <ul class="commentsContainer">
           <li class="comment"></li>
-      </ul>
-    </div>
-    <form action="post">
-      <input type="text" placeholder="Your Name">
-      <textarea name="text" id="insights" cols="30" rows="10" placeholder="Your insights"></textarea>
-      <input type="button" value="Comment">
-    </form>
-  </div>`;
+       </ul>
+     </div>
+     <form action="post">
+       <input type="text" placeholder="Your Name">
+       <textarea name="text" id="insights" cols="30" rows="10" placeholder="Your insights"></textarea>
+       <input type="button" value="Comment">
+     </form>
+   </div>`;
   main.appendChild(popupDiv);
 }
 
 function displayImage() {
   getScores(getImage)
-    .then((data) => {
-      displayPopup(data.hdurl, data.title, data.explanation);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    .then((data) => displayPopup(data.hdurl, data.title, data.explanation))
+    .catch((error) => console.log(error));
 }
-
-displayImage();
