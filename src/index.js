6982a4b3ef5ace00521d8be6093bea61e2f1dc85
Gints Misins <88575586+GintsM@users.noreply.github.com>
2021-11-23 15:45:28 +0200

    Home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import './style.css';
+
+const getLink = 'Hi Aku and Zachee';
+const main = document.getElementById('addToScreen');
+
+const getScores = async (url) => {
+  const response = await fetch(url);
+  return response.json();
+};
+
+function addToScoreBord(img, title) {
+  const div = document.createElement('div');
+  div.classList.add('cardContainer');
+  div.innerHTML = `
+        <div class="imgcontainer">
+          <img src="${img}" alt="">
+        </div>
+        <div class="title">
+          <h3>${title}</h3>
+          <span><i class="far fa-star"></i>
+          </span>
+          <small>0 likes</small> 
+        </div>
+        <input type="button" value="Comments">
+        `;
+  main.appendChild(div);
+}
+
+function displayScores() {
+  getScores(getLink)
+    .then((data) => {
+      main.innerHTML = '';
+      data.forEach((elem) => addToScoreBord(elem.hdurl, elem.title));
+    });
+}
+
+displayScores();
