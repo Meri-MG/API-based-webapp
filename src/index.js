ea8f6f2becd2e1c101acd1332410109448d0905f
Meri <mgogi13@freeuni.edu.ge>
2021-11-23 18:26:14 +0400

    Create popup window

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 
 const getLink = 'Hi Aku and Zachee';
+const getImage = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2017-07-08';
 const main = document.getElementById('addToScreen');
 
 const getScores = async (url) => {
@@ -35,3 +36,35 @@ function displayScores() {
 }
 
 displayScores();
+
+function displayPopup(img, title, description) {
+  const popupDiv = document.createElement('div');
+  popupDiv.classList.add('popupContainer');
+  popupDiv.innerHTML = `<div class="header-popup">
+    <img src="${img}" class="whatever" alt="close-icon">
+  </div>
+  <h2>${title}</h2>
+  <p class="description">${description}</p>
+  <div>
+      <h2 class="comments">Comments</h2>
+      <ul class="commentsContainer">
+          <li class="comment"></li>
+      </ul>
+    </div>
+    <form action="post">
+      <input type="text" placeholder="Your Name">
+      <textarea name="text" id="insights" cols="30" rows="10" placeholder="Your insights"></textarea>
+      <input type="button" value="Comment">
+    </form>
+  </div>`;
+  main.appendChild(popupDiv);
+}
+
+function displayImage() {
+  getScores(getImage)
+    .then((data) => {
+      displayPopup(data.hdurl, data.title, data.explanation);
+    });
+}
+
+displayImage();
