e4924c660f7e0f7866aa4155e30acde8589d0880
Gints Misins <88575586+GintsM@users.noreply.github.com>
2021-11-25 09:10:03 +0200

    Involvement API in homepage

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import './style.css';
 const getLink = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2020-07-15&end_date=2020-07-20';
 const getImage = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-07-21';
 const main = document.getElementById('addToScreen');
+const starLink = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/9mAPgvMc6PjOJk4JU1ZU/likes/';
+const commentLink = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/9mAPgvMc6PjOJk4JU1ZU/comments';
 
 const getScores = async (url) => {
   const response = await fetch(url);
   return response.json();
 };
+
+const postScores = async (url, data) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+};
+
+// to give a star
+
+
 
 function addToScoreBord(img, title, index) {
   const div = document.createElement('div');
@@ -18,9 +35,9 @@ function addToScoreBord(img, title, index) {
         </div>
         <div class="title">
           <h3>${title}</h3>
-          <a href="#"><i class="far fa-star"></i></a>
+          <a href="#" id="${index}star" class="stars"><i class="far fa-star"></i></a>
         </div>
-        <small>0 likes</small> 
+        <small>0 <i>likes</i></small> 
         <input type="button" value="Comments" id="${index}" class="comment">
         `;
   main.appendChild(div);
@@ -68,13 +85,40 @@ function displayImage() {
     .catch((error) => console.log(error));
 }
 
+// stars must be number or string
+function increaseStars(element, stars) {
+  // const small = parseInt(element.parentElement.nextSibling.innerText)+1;// Check this !!!!!!
+  element.parentElement.nextSibling.innerHTML = stars;
+}
+
+// WORK ON THIS
+function displayStars() {
+  getScores(starLink)
+    .then((data) => data.forEach((elem, index) => increaseStars(elem.hdurl, elem.title, index)))
+}
+
 function displayScores() {
   getScores(getLink)
     .then((data) => data.forEach((elem, index) => addToScoreBord(elem.hdurl, elem.title, index)))
     .then(() => {
-      const lastComment = document.getElementById('5');
-      lastComment.addEventListener('click', displayImage);
-    });
+      // WE must to give proper count of our stars 
+      displayStars();// function GET      
+    })
 }
 
 displayScores();
+
+function giveStar(id, element) {
+  postScores(starLink, data = {"item_id": id})
+    .then(increaseStars(element));
+}
+
+
+main.addEventListener('click', (e) => {
+  if (e.target.classList.contains('stars')) {
+    giveStar(e.target.id, e.target);
+  }
+  if (e.target.classList.contains('comment')) {
+    displayImage();
+  }  
+})
