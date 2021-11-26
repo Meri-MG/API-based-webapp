7855edacfd938c628ff95b38098f09518a43189e
Gints Misins <gintsmis@gmail.com>
2021-11-26 09:48:00 +0200

    Stars (likes) in homepage and add stars
    * Involvement API in homepage
    
    * Stars(alias likes) on homepage
    
    * Stars(like), add after confirm request
    
    * Remove consol and white spaces

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import './style.css';
 const getLink = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2020-07-15&end_date=2020-07-20';
 const getImage = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-07-21';
 const main = document.getElementById('addToScreen');
+const starLink = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/9mAPgvMc6PjOJk4JU1ZU/likes/';
+// const commentLink = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/9mAPgvMc6PjOJk4JU1ZU/comments';
 
 const getScores = async (url) => {
   const response = await fetch(url);
   return response.json();
+};
+
+const postScores = async (url, data) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  return response;
 };
 
 function addToScoreBord(img, title, index) {
@@ -18,9 +31,9 @@ function addToScoreBord(img, title, index) {
         </div>
         <div class="title">
           <h3>${title}</h3>
-          <a href="#"><i class="far fa-star"></i></a>
+          <a href="#" id="${index}star" class="stars"><i class="far fa-star"></i></a>
         </div>
-        <small>0 likes</small> 
+        <small></small>
         <input type="button" value="Comments" id="${index}" class="comment">
         `;
   main.appendChild(div);
@@ -64,17 +77,45 @@ function displayImage() {
       closeBtn.addEventListener('click', () => {
         closePopup(closeBtn);
       });
-    })
-    .catch((error) => console.log(error));
+    });
+}
+
+const splitStars = (id, stars) => {
+  const small = document.getElementById(id);
+  small.parentElement.nextElementSibling.innerHTML = `${stars} stars`;
+};
+
+function displayStars() {
+  getScores(starLink)
+    .then((data) => data.forEach((elem) => splitStars(elem.item_id, elem.likes)));
 }
 
 function displayScores() {
   getScores(getLink)
     .then((data) => data.forEach((elem, index) => addToScoreBord(elem.hdurl, elem.title, index)))
-    .then(() => {
-      const lastComment = document.getElementById('5');
-      lastComment.addEventListener('click', displayImage);
+    .then(() => displayStars());
+}
+
+function giveStar(id, stars) {
+  const data = { item_id: id };
+  postScores(starLink, data)
+    .then((data) => {
+      if (data.status === 201) {
+        splitStars(id, stars);
+      }
     });
 }
 
 displayScores();
+
+main.addEventListener('click', (e) => {
+  if (e.target.classList.contains('fa-star')) {
+    e.preventDefault();
+    const sC = parseInt(e.target.parentElement.parentElement.nextElementSibling.textContent, 10);
+    const stars = sC + 1;
+    giveStar(e.target.parentElement.id, stars);
+  }
+  if (e.target.classList.contains('comment')) {
+    displayImage();
+  }
+});
